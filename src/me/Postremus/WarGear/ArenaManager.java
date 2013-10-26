8f03d590c9c7bfaa1a5a49f3c00d8b75870d406b
Postremus <panzer1996@googlemail.com>
2013-10-26 09:48:58 +0200

    Arenamanger erstellt Dieser kümert sich um das laden und finden von arenen

---
+++ src/me/Postremus/WarGear/ArenaManager.java
@@ -0,0 +1,50 @@
+package me.Postremus.WarGear;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArenaManager {
+
+	private WarGear plugin;
+	private List<Arena> arenas;
+	
+	public ArenaManager(WarGear plugin)
+	{
+		this.plugin = plugin;
+		this.arenas = new ArrayList<Arena>();
+		this.loadArenas();
+	}
+	
+	public void loadArenas()
+	{
+		for (String element : this.plugin.getRepo().getArenaNames())
+		{
+			this.arenas.add(new Arena(this.plugin, element));
+		}
+	}
+	
+	public Arena getArena(String name)
+	{
+		for (Arena element : arenas)
+		{
+			if (element.getArenaName().equalsIgnoreCase(name))
+			{
+				return element;
+			}
+		}
+		return getDefaultArena();
+	}
+	
+	public Arena getDefaultArena()
+	{
+		String defaultName = this.plugin.getRepo().getDefaultArenaName();
+		for (Arena element : arenas)
+		{
+			if (element.getArenaName().equalsIgnoreCase(defaultName))
+			{
+				return element;
+			}
+		}
+		return null;
+	}
+}
