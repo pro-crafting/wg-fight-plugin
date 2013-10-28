d20714008334938ed7c16ef1c3bd610dd0dc1e96
Postremus <panzer1996@googlemail.com>
2013-10-28 22:04:42 +0100

    Permissions hinzugefügt

--- src/me/Postremus/WarGear/WgkCommand.java
+++ src/me/Postremus/WarGear/WgkCommand.java
@@ -24,25 +24,25 @@ public class WgkCommand implements CommandExecutor{
 			String[] args) {
 		if (args.length == 0)
 		{
-			Help(sender);
+			help(sender);
 		}
 		if (args.length > 0)
 		{
 			if (args.length == 1)
 			{
-				if (args[0].equalsIgnoreCase("start"))
+				if (args[0].equalsIgnoreCase("start") && sender.hasPermission("wargear.fight.start"))
 				{
 					this.logik.start(sender);
 				}
-				else if (args[0].equalsIgnoreCase("count"))
+				else if (args[0].equalsIgnoreCase("count") && sender.hasPermission("wargear.count"))
 				{
 					this.logik.StartManuelCountdown();
 				}
-				else if (args[0].equalsIgnoreCase("setup"))
+				else if (args[0].equalsIgnoreCase("setup") && sender.hasPermission("wargear.fight.setup"))
 				{
 					this.logik.setup(sender);
 				}
-				else if (args[0].equalsIgnoreCase("reload"))
+				else if (args[0].equalsIgnoreCase("reload") && sender.hasPermission("wargear.reload"))
 				{
 					this.plugin.reloadConfig();
 					this.plugin.getServer().getPluginManager().disablePlugin(plugin);
@@ -51,41 +51,44 @@ public class WgkCommand implements CommandExecutor{
 				}
 				else
 				{
-					Help(sender);
+					help(sender);
 				}
 			}
 			if (args.length > 1)
 			{
-				if (args[0].equalsIgnoreCase("setup") && args.length == 2)
+				if (args[0].equalsIgnoreCase("setup") && args.length == 2 && sender.hasPermission("wargear.fight.start"))
 				{
 					this.logik.setup(sender, args[1]);
 				}
-				else if (args[0].equalsIgnoreCase("team1"))
+				else if (args[0].equalsIgnoreCase("team1") && sender.hasPermission("wargear.fight.team1"))
 				{
 					List<String> teamMember = Arrays.asList(args);
 					teamMember = teamMember.subList(1, teamMember.size());
 					this.logik.setTeam(sender, "team1", teamMember);	
 				}
-				else if (args[0].equalsIgnoreCase("team2"))
+				else if (args[0].equalsIgnoreCase("team2") && sender.hasPermission("wargear.fight.team2"))
 				{
 					List<String> teamMember = Arrays.asList(args);
 					teamMember = teamMember.subList(1, teamMember.size());
 					this.logik.setTeam(sender, "team2", teamMember);	
 				}
-				else if (args[0].equalsIgnoreCase("kit"))
+				else if (args[0].equalsIgnoreCase("kit") && sender.hasPermission("wargear.fight.kit"))
 				{
 					this.logik.setKit(sender, args[1]);
 				}
-				else if (args[0].equalsIgnoreCase("quit"))
+				else if (args[0].equalsIgnoreCase("quit") && sender.hasPermission("wargear.fight.quit"))
 				{
 					if  ((!args[1].equalsIgnoreCase("team1")) && (!args[1].equalsIgnoreCase("team2")))
 					{
-						Help(sender);
+						help(sender);
 						return true;
 					}
-					this.logik.quit(sender, args[1]);
+					else
+					{
+						this.logik.quit(sender, args[1]);
+					}
 				}
-				else if (args[0].equalsIgnoreCase("mode"))
+				else if (args[0].equalsIgnoreCase("mode") && sender.hasPermission("wargear.fight.start"))
 				{
 					this.logik.setMode(sender, args[1]);
 				}
@@ -109,33 +112,33 @@ public class WgkCommand implements CommandExecutor{
 						sender.sendMessage("Die Arena "+ arenaName+" existiert nicht.");
 						return true;
 					}
-					if (args[1].equalsIgnoreCase("open"))
+					if (args[1].equalsIgnoreCase("open") && sender.hasPermission("wargear.arena.open"))
 					{
 						this.logik.getArenaManager().getArena(arenaName).open();
 					}
-					else if (args[1].equalsIgnoreCase("close"))
+					else if (args[1].equalsIgnoreCase("close") && sender.hasPermission("wargear.arena.close"))
 					{
 						this.logik.getArenaManager().getArena(arenaName).close();
 					}
-					else if (args[1].equalsIgnoreCase("list"))
+					else if (args[1].equalsIgnoreCase("list") && sender.hasPermission("wargear.arena.list"))
 					{
 						this.logik.showArenaNames(sender);
 					}
 					else
 					{
-						Help(sender);
+						help(sender);
 					}
 				}
 				else
 				{
-					Help(sender);
+					help(sender);
 				}
 			}
 		}
 		return true;
 	}
 	
-	private void Help(CommandSender sender)
+	private void help(CommandSender sender)
 	{
 		sender.sendMessage("Kein passender Befehl gefunden!");
 		sender.sendMessage("/wgk setup [arena]");
