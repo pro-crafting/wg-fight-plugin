caeb682f14f5b4cdced6d01826ac26ff4f39ae16
Postremus <panzer1996@googlemail.com>
2014-03-29 10:12:40 +0100

    lookat und move hinzugefügt

--- src/me/Postremus/WarGear/WarGearUtil.java
+++ src/me/Postremus/WarGear/WarGearUtil.java
@@ -8,6 +8,9 @@ import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
+
+import com.sk89q.worldedit.bukkit.BukkitUtil;
 
 import me.Postremus.WarGear.Arena.Arena;
 
@@ -60,8 +63,7 @@ public class WarGearUtil {
 			}
 			ret.add(argument);
 		}
-		String[] retType = new String[0];
-		return ret.toArray(retType);
+		return ret.toArray(new String[0]);
 	}
 	
 	public static void makeHealthy(Player player)
@@ -92,4 +94,48 @@ public class WarGearUtil {
 		player.setAllowFlight(true);
 		player.setFlying(true);
 	}
+	
+	public static Location move(Location start, int distance)
+	{
+		Location ret = start.clone();
+		ret.add(ret.getDirection().normalize().multiply(distance).toLocation(start.getWorld()));
+		return ret;
+	}
+	
+	//Lookat function of bergerkiller
+	//https://forums.bukkit.org/threads/lookat-and-move-functions.26768/
+	public static Location lookAt(Location loc, Location lookat) {
+        //Clone the loc to prevent applied changes to the input loc
+        loc = loc.clone();
+
+        // Values of change in distance (make it relative)
+        double dx = lookat.getX() - loc.getX();
+        double dy = lookat.getY() - loc.getY();
+        double dz = lookat.getZ() - loc.getZ();
+
+        // Set yaw
+        if (dx != 0) {
+            // Set yaw start value based on dx
+            if (dx < 0) {
+                loc.setYaw((float) (1.5 * Math.PI));
+            } else {
+                loc.setYaw((float) (0.5 * Math.PI));
+            }
+            loc.setYaw((float) loc.getYaw() - (float) Math.atan(dz / dx));
+        } else if (dz < 0) {
+            loc.setYaw((float) Math.PI);
+        }
+
+        // Get the distance from dx/dz
+        double dxz = Math.sqrt(Math.pow(dx, 2) + Math.pow(dz, 2));
+
+        // Set pitch
+        loc.setPitch((float) -Math.atan(dy / dxz));
+
+        // Set values, convert to degrees (invert the yaw since Bukkit uses a different yaw dimension format)
+        loc.setYaw(-loc.getYaw() * 180f / (float) Math.PI);
+        loc.setPitch(loc.getPitch() * 180f / (float) Math.PI);
+
+        return loc;
+    }
 }
