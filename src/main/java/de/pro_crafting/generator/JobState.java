64cc20598ddf7c72a86be3c5f8799c85166f9832
Postremus <postremus1996@googlemail.com>
2014-07-06 14:41:07 +0200

    BlockGenerator and commandframework are now dependencys from maven

--- src/main/java/de/pro_crafting/generator/JobState.java
+++
@@ -1,9 +0,0 @@
-package de.pro_crafting.generator;
-
-public enum JobState 
-{
-	Unstarted,
-	Started,
-	Paused,
-	Finished
-}
--- src/main/java/de/pro_crafting/generator/JobStateChangedCallback.java
+++
@@ -1,7 +0,0 @@
-package de.pro_crafting.generator;
-
-
-public interface JobStateChangedCallback 
-{
-	public void jobStateChanged(Job job, JobState fromState);
-}
