75033d1bf6c13d885de966d390fbd16f464e3f9a
Martin Panzer <postremus1996@googlemail.com>
2016-01-19 18:58:05 +0100

    we now equalize players only based on the uuid because only one player per uuid can be only we don't need to check the other attributes to make sure we found the right member

--- src/main/java/de/pro_crafting/wg/group/GroupMember.java
+++ src/main/java/de/pro_crafting/wg/group/GroupMember.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 public class GroupMember {
 	private UUID playerId;
 	private Boolean alive;
+
 	private Boolean isLeader;
 	
 	public GroupMember(Player player, boolean isTeamLeader) {
@@ -44,46 +45,20 @@ public class GroupMember {
 	public String getName() {
 		return this.isOnline() ? this.getPlayer().getDisplayName() : this.getOfflinePlayer().getName();
 	}
-	
+
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((this.alive == null) ? 0 : this.alive.hashCode());
-		result = prime
-				* result
-				+ ((this.isLeader == null) ? 0 : this.isLeader
-						.hashCode());
-		result = prime * result
-				+ ((this.playerId == null) ? 0 : this.playerId.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		GroupMember that = (GroupMember) o;
+
+		return playerId.equals(that.playerId);
+
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GroupMember other = (GroupMember) obj;
-		if (this.alive == null) {
-			if (other.alive != null)
-				return false;
-		} else if (!this.alive.equals(other.alive))
-			return false;
-		if (this.isLeader == null) {
-			if (other.isLeader != null)
-				return false;
-		} else if (!this.isLeader.equals(other.isLeader))
-			return false;
-		if (this.playerId == null) {
-			if (other.playerId != null)
-				return false;
-		} else if (!this.playerId.equals(other.playerId))
-			return false;
-		return true;
+	public int hashCode() {
+		return playerId.hashCode();
 	}
 }
