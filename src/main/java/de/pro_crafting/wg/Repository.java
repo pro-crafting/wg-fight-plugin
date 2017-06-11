7d9ccdfb2c57e03aaef8ae9e7d78a54d65214020
Martin Panzer <postremus1996@googlemail.com>
2017-06-11 13:04:45 +0200

    Format complete project using google code format

--- src/main/java/de/pro_crafting/wg/Repository.java
+++ src/main/java/de/pro_crafting/wg/Repository.java
@@ -1,133 +1,112 @@
 package de.pro_crafting.wg;
+
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import de.pro_crafting.kit.KitProvider;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.plugin.RegisteredServiceProvider;
 
-import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-
-import de.pro_crafting.kit.KitProvider;
-import de.pro_crafting.region.RegionManager;
-import de.pro_crafting.region.RegionsLibrary;
-
 public class Repository {
-	private WarGear plugin;
-	private Economy eco;
-	
-	public Repository(WarGear plugin)
-	{
-		this.plugin = plugin;
-		this.eco = loadEco();
-	}
-	
-	public String getDefaultKitName()
-	{
-		return this.plugin.getConfig().getString("kit");
-	}
 
-	public boolean isEconomyEnabled()
-	{
-		return this.plugin.getConfig().getBoolean("economy.enabled", false);
-	}
-	
-	public double getWinAmount()
-	{
-		return this.plugin.getConfig().getDouble("economy.win", 2.5);
-	}
-	
-	public double getLoseAmount()
-	{
-		return this.plugin.getConfig().getDouble("economy.lose", -2.5);
-	}
-	
-	public double getDrawAmount()
-	{
-		return this.plugin.getConfig().getDouble("economy.draw", 1);
-	}
-	
-	public boolean isPrefixEnabled()
-	{
-		return this.plugin.getConfig().getBoolean("prefix-enabled", true);
-	}
-	
-	public boolean areMetricsEnabled()
-	{
-		return this.plugin.getConfig().getBoolean("metrics", false);
-	}
-	
-	public boolean isUpdateCheckEnabled()
-	{
-		return this.plugin.getConfig().getBoolean("update-check", false);
-	}
-	
-	public int getOfflineKickTime()
-	{
-		return this.plugin.getConfig().getInt("offline-kick-time", 30);
-	}
+  private WarGear plugin;
+  private Economy eco;
 
-	public boolean isGroupChatEnabled() {
-		return this.plugin.getConfig().getBoolean("group-chat.enabled", true);
-	}
+  public Repository(WarGear plugin) {
+    this.plugin = plugin;
+    this.eco = loadEco();
+  }
 
-	public String getGroupChatSign() {
-		return this.plugin.getConfig().getString("group-chat.sign", "#");
-	}
+  public String getDefaultKitName() {
+    return this.plugin.getConfig().getString("kit");
+  }
 
-	public String getGroupChatFormat() {
-		return this.plugin.getConfig().getString("group-chat.format", "%gc>§7%displayname: %message");
-	}
+  public boolean isEconomyEnabled() {
+    return this.plugin.getConfig().getBoolean("economy.enabled", false);
+  }
 
-	public WorldEditPlugin getWorldEdit()
-	{
-		return (WorldEditPlugin)this.plugin.getServer().getPluginManager().getPlugin("WorldEdit");
-	}
-	
-	public Economy getEco()
-	{
-		return this.eco;
-	}
-	
-	private Economy loadEco()
-	{
-		if (this.plugin.getServer().getPluginManager().getPlugin("Vault") == null)
-		{
-			return null;
-		}
-		RegisteredServiceProvider<Economy> economyProvider = this.plugin.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
-        if (economyProvider != null) {
-        	return economyProvider.getProvider();
-        }
-        return null;
-	}
-	
-	public KitProvider getKit()
-	{
-		return this.getKitProvider();
-	}
-	
-	private KitProvider getKitProvider()
-	{
-		RegisteredServiceProvider<KitProvider> provider = this.plugin.getServer().getServicesManager().getRegistration(KitProvider.class);
-        if (provider != null) {
-        	return provider.getProvider();
-        }
-        return null;
-	}
-	
-	public List<String> getArenaNames()
-	{
-		List<String> ret = new ArrayList<String>();
-		for (File fileEntry : this.plugin.getArenaFolder().listFiles()) 
-		{
-			if (fileEntry.isFile())
-			{
-				String arenaName = fileEntry.getName().substring(0, fileEntry.getName().indexOf("."));
-				ret.add(arenaName);
-			}
-		}
-		return ret;
-	}
+  public double getWinAmount() {
+    return this.plugin.getConfig().getDouble("economy.win", 2.5);
+  }
+
+  public double getLoseAmount() {
+    return this.plugin.getConfig().getDouble("economy.lose", -2.5);
+  }
+
+  public double getDrawAmount() {
+    return this.plugin.getConfig().getDouble("economy.draw", 1);
+  }
+
+  public boolean isPrefixEnabled() {
+    return this.plugin.getConfig().getBoolean("prefix-enabled", true);
+  }
+
+  public boolean areMetricsEnabled() {
+    return this.plugin.getConfig().getBoolean("metrics", false);
+  }
+
+  public boolean isUpdateCheckEnabled() {
+    return this.plugin.getConfig().getBoolean("update-check", false);
+  }
+
+  public int getOfflineKickTime() {
+    return this.plugin.getConfig().getInt("offline-kick-time", 30);
+  }
+
+  public boolean isGroupChatEnabled() {
+    return this.plugin.getConfig().getBoolean("group-chat.enabled", true);
+  }
+
+  public String getGroupChatSign() {
+    return this.plugin.getConfig().getString("group-chat.sign", "#");
+  }
+
+  public String getGroupChatFormat() {
+    return this.plugin.getConfig().getString("group-chat.format", "%gc>§7%displayname: %message");
+  }
+
+  public WorldEditPlugin getWorldEdit() {
+    return (WorldEditPlugin) this.plugin.getServer().getPluginManager().getPlugin("WorldEdit");
+  }
+
+  public Economy getEco() {
+    return this.eco;
+  }
+
+  private Economy loadEco() {
+    if (this.plugin.getServer().getPluginManager().getPlugin("Vault") == null) {
+      return null;
+    }
+    RegisteredServiceProvider<Economy> economyProvider = this.plugin.getServer()
+        .getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+    if (economyProvider != null) {
+      return economyProvider.getProvider();
+    }
+    return null;
+  }
+
+  public KitProvider getKit() {
+    return this.getKitProvider();
+  }
+
+  private KitProvider getKitProvider() {
+    RegisteredServiceProvider<KitProvider> provider = this.plugin.getServer().getServicesManager()
+        .getRegistration(KitProvider.class);
+    if (provider != null) {
+      return provider.getProvider();
+    }
+    return null;
+  }
+
+  public List<String> getArenaNames() {
+    List<String> ret = new ArrayList<>();
+    for (File fileEntry : this.plugin.getArenaFolder().listFiles()) {
+      if (fileEntry.isFile()) {
+        String arenaName = fileEntry.getName().substring(0, fileEntry.getName().indexOf("."));
+        ret.add(arenaName);
+      }
+    }
+    return ret;
+  }
 }
--- src/main/java/de/pro_crafting/wg/group/Group.java
+++ src/main/java/de/pro_crafting/wg/group/Group.java
@@ -4,129 +4,134 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
-public class Group  {
-	private Map<UUID, GroupMember> member;
-	private boolean isReady;
-	protected PlayerRole role;
-	private int cannons;
-	
-	public Group(PlayerRole role) {
-		this.role = role;
-		this.isReady = false;
-		this.member = new HashMap<UUID, GroupMember>();
-	}
-	
-	public void add(Player p, boolean isLeader) {
-		this.member.put(p.getUniqueId(), new GroupMember(p, isLeader));
-	}
-	
-	public void remove(OfflinePlayer p) {
-		this.member.remove(p.getUniqueId());
-	}
-	
-	public boolean isReady() {
-		return this.isReady;
-	}
-	
-	public void setIsReady(boolean isReady) {
-		this.isReady = isReady;
-	}
-	
-	public PlayerRole getRole() {
-		return this.role;
-	}
-	
-	public Collection<GroupMember> getMembers() {
-		return this.member.values();
-	}
-	
-	public GroupMember getMember(OfflinePlayer player) {
-		return this.member.get(player.getUniqueId());
-	}
-	
-	public boolean isAlive() {
-		for (GroupMember current : this.member.values()) {
-			if (current.isAlive()) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean hasLeader() {
-		return getLeader() != null;
-	}
-	
-	public GroupMember getLeader() {
-		for (GroupMember current : this.member.values()) {
-			if (current.isLeader()) {
-				return current;
-			}
-		}
-		return null;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (isReady ? 1231 : 1237);
-		result = prime * result
-				+ ((member == null) ? 0 : member.hashCode());
-		result = prime * result
-				+ ((role == null) ? 0 : role.hashCode());
-		return result;
-	}
+public class Group {
 
-	public int getCannons() {
-		return this.cannons;
-	}
+  protected PlayerRole role;
+  private Map<UUID, GroupMember> member;
+  private boolean isReady;
+  private int cannons;
 
-	public void setCannons(int cannons) {
-		this.cannons = cannons;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Group other = (Group) obj;
-		if (isReady != other.isReady)
-			return false;
-		if (member == null) {
-			if (other.member != null)
-				return false;
-		} else if (!member.equals(other.member))
-			return false;
-		if (role != other.role)
-			return false;
-		return true;
-	}
+  public Group(PlayerRole role) {
+    this.role = role;
+    this.isReady = false;
+    this.member = new HashMap<>();
+  }
 
-	public boolean isOnline() {
-		for (GroupMember member : this.member.values())
-		{
-			if (!member.isOnline())
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+  public void add(Player p, boolean isLeader) {
+    this.member.put(p.getUniqueId(), new GroupMember(p, isLeader));
+  }
 
-	public void broadcast(String message) {
-		for (GroupMember groupMember : this.member.values()) {
-			if (groupMember.isOnline()) {
-				groupMember.getPlayer().sendMessage(message);
-			}
-		}
-	}
+  public void remove(OfflinePlayer p) {
+    this.member.remove(p.getUniqueId());
+  }
+
+  public boolean isReady() {
+    return this.isReady;
+  }
+
+  public void setIsReady(boolean isReady) {
+    this.isReady = isReady;
+  }
+
+  public PlayerRole getRole() {
+    return this.role;
+  }
+
+  public Collection<GroupMember> getMembers() {
+    return this.member.values();
+  }
+
+  public GroupMember getMember(OfflinePlayer player) {
+    return this.member.get(player.getUniqueId());
+  }
+
+  public boolean isAlive() {
+    for (GroupMember current : this.member.values()) {
+      if (current.isAlive()) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean hasLeader() {
+    return getLeader() != null;
+  }
+
+  public GroupMember getLeader() {
+    for (GroupMember current : this.member.values()) {
+      if (current.isLeader()) {
+        return current;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (isReady ? 1231 : 1237);
+    result = prime * result
+        + ((member == null) ? 0 : member.hashCode());
+    result = prime * result
+        + ((role == null) ? 0 : role.hashCode());
+    return result;
+  }
+
+  public int getCannons() {
+    return this.cannons;
+  }
+
+  public void setCannons(int cannons) {
+    this.cannons = cannons;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Group other = (Group) obj;
+    if (isReady != other.isReady) {
+      return false;
+    }
+    if (member == null) {
+      if (other.member != null) {
+        return false;
+      }
+    } else if (!member.equals(other.member)) {
+      return false;
+    }
+    if (role != other.role) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean isOnline() {
+    for (GroupMember member : this.member.values()) {
+      if (!member.isOnline()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void broadcast(String message) {
+    for (GroupMember groupMember : this.member.values()) {
+      if (groupMember.isOnline()) {
+        groupMember.getPlayer().sendMessage(message);
+      }
+    }
+  }
 }
